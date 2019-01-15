7255b3a4aef2e711a3d1eb95df0b145b44493f2f
MithiMore <mithi.more1@gmail.com>
2019-01-14 20:25:55 -0800

    Add files via upload

---
+++ exampleArrays.java
@@ -0,0 +1,15 @@
+
+
+class exampleArrays {
+	
+	
+	public static void main(String[] args){
+		int[] A = {1,2,3,4,5};
+		String[] B = {"a", "b", "c", "d"};
+		
+		for (int i = 0; i < A.length; i++) {
+			System.out.println(A[i]);
+			System.out.println(B[i]);
+		}
+	}
+}
